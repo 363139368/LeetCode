5a62d8bdd74f07c5caf46600aa016a2b468269fd
宋小华 <363139368@qq.com>
2020-07-10 21:11:33 +0800

    1

---
+++ LeetCode/3无重复字符的最长子串.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _3无重复字符的最长子串
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int sum = 0;
+            int start = 0;
+            int end = 0;
+
+            return sum;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,6 +10,22 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            int a = 1;
+            int b = 1 << 1;
+            int c = 1 << 2;
+            int d = 1 << 3;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            int res = 0;
+            res |= b;
+            res |= b;
+            res |= c;
+            Console.WriteLine(res);
+
+            Console.WriteLine((res & b) == b);
+            Console.ReadLine();
         }
     }
 }
