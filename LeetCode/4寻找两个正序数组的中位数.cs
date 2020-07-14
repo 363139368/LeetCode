fb41a428fb3410710be25a8a07ab521e15e41ffb
宋小华 <363139368@qq.com>
2020-07-14 20:49:47 +0800

    _4寻找两个正序数组的中位数

---
+++ LeetCode/4寻找两个正序数组的中位数.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _4寻找两个正序数组的中位数
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int a = int.MinValue;
+            return 0;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,36 +10,19 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine((int)(1 << 16));
-            //Console.ReadLine();
-
-            //return;
-            //for (int i = 0; i < 128; i++)
-            //{
-            //    Console.WriteLine(i.ToString() + " : " + (1 << i).ToString());
-            //}
-            //Console.ReadLine();
-            //return;
-            _3无重复字符的最长子串 temp = new _3无重复字符的最长子串();
-            Console.WriteLine(temp.LengthOfLongestSubstring("bbbbb"));
-            Console.ReadLine();
-
-            return;
-            int a = 1;
-            int b = 1 << 1;
-            int c = 1 << 2;
-            int d = 1 << 3;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            int res = 0;
-            res |= b;
-            res |= c;
-            Console.WriteLine(res);
-
-            Console.WriteLine((res & b) == b);
-            Console.ReadLine();
+            {
+                _4寻找两个正序数组的中位数 _4 = new _4寻找两个正序数组的中位数();
+                var temp = _4.FindMedianSortedArrays(new int[] { 4 }, new int[] { 1,2,3 });
+                Console.WriteLine(temp);
+                Console.ReadLine();
+                return;
+            }
+            {
+                _3无重复字符的最长子串 _3 = new _3无重复字符的最长子串();
+                Console.WriteLine(_3.LengthOfLongestSubstring("bbbbb"));
+                Console.ReadLine();
+                return;
+            }
         }
     }
 }
