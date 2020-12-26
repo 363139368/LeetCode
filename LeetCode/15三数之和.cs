725b8a2b5f9d6f1906d7bf87a9325860118e6fa0
sdh <363139368@qq.com>
2020-12-26 12:43:32 +0800

    add Signed-off-by: sdh <363139368@qq.com>

---
+++ LeetCode/15三数之和.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _15三数之和
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            if (nums.Length < 3)
+                return null;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetCode/84柱状图中最大的矩形.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _84柱状图中最大的矩形
+    {
+        public int LargestRectangleArea(int[] heights)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            HashSet<int> ignore = new HashSet<int>();
+            int minValue = 0;
+            int max = 0;
+            for (int i = 0, weight = 0; i < heights.Length; i++)
+            {
+                var cur = heights[i];
+                if (ignore.Contains(cur))
+                    continue;
+                weight++;
+                if (dic.TryGetValue(cur, out var count))
+                    dic[cur] = count + 1;
+                else
+                    dic[cur] = 1;
+
+                var itor = dic.GetEnumerator();
+                while (itor.MoveNext())
+                {
+
+                }
+            }
+            return 0;
+        }
+    }
+}
