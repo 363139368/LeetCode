c7f1beb8c2bc0c9864e7c2248cffe1e97d80d606
sdh <363139368@qq.com>
2021-01-20 00:18:00 +0800

    1
    Signed-off-by: sdh <363139368@qq.com>

---
+++ LeetCode/142环形链表II.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class _142环形链表II
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+
+            do
+            {
+                if (fast == null || fast.next == null)
+                    return null;
+                fast = fast.next.next;
+                slow = slow.next;
+            } while (slow != fast);
+            fast = head;
+            while (fast != slow)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return fast;
+        }
+    }
+}
