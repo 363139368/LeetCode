9ac767c965edbb205e0f627c0adad7029878a0a2
宋小华 <363139368@qq.com>
2025-02-19 02:40:33 +0800

    1

---
+++ LeetCode/LCR004只出现一次的数字II.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LCR004只出现一次的数字II
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Dictionary<int,bool> dic = new Dictionary<int,bool>(nums.Length /3);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.TryGetValue(nums[i], out var b))
+                {
+                    if (b)
+                    {
+                        dic[nums[i]] = false;
+                    }
+                    else
+                    {
+                        dic.Remove(nums[i]);
+                    }
+                }
+                else
+                {
+                    dic[nums[i]] = true;
+                }
+
+            }
+            var ator = dic.GetEnumerator();
+            while (ator.MoveNext())
+            {
+                return ator.Current.Key;
+            }
+            return 0;
+        }
+
+        public int BitSingleNumber(int[] nums)
+        {
+            int[] res = new int[32];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < 32; j++)
+                {
+                    if ((nums[i] >> j & 1) > 0)
+                    {
+                        res[j]++;
+                    }
+                }
+            }
+
+            int ant = 0;
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (res[i] > 0 && res[i] % 3 > 0)
+                {
+                    ant += 1 << i;
+                }
+            }
+            return ant;
+        }
+    }
+}
---
+++ LeetCode/LCR006两数之和II输入有序数组.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LCR006两数之和II输入有序数组
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int i = 0;
+            int j = numbers.Length - 1;
+            while (true)
+            {
+                var tempSum = numbers[i] + numbers[j];
+                if (tempSum == target)
+                {
+                    return new int[2] {i,j};
+                }
+                else if (tempSum > target)
+                {
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
