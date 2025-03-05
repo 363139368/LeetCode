e92e752db744d00659612eb2064bfbd68a1db9a0
宋小华 <363139368@qq.com>
2025-03-05 22:40:04 +0800

    1

---
+++ LeetCode/290单词规律.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _290单词规律
+    {
+        public class Solution
+        {
+            public bool WordPattern(string pattern, string s)
+            {
+                Dictionary<string, int> dic = new Dictionary<string, int>();
+                var arr = s.Split(' ');
+                int tmp = 1;
+                int[] ints = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (!dic.ContainsKey(arr[i]))
+                    {
+                        dic[arr[i]] = tmp;
+                        tmp++;
+                    }
+
+                    ints[i] = dic[arr[i]];
+                }
+                if (pattern.Length != ints.Length) return false;
+
+                Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+                tmp = 1;
+                for (int i = 0; i < pattern.Length; i++)
+                {
+                    var cur = pattern[i];
+                    if (!keyValuePairs.ContainsKey(cur))
+                    {
+                        keyValuePairs[cur] = tmp;
+                        if (ints[i] != tmp)
+                        {
+                            return false;
+                        }
+                        tmp++;
+                    }
+                    else
+                    {
+                        if (ints[i] != keyValuePairs[cur])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ LeetCode/532数组中的k_diff数对.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    // 1 2 3 4 5 6 7 8  k=3
+    public class _532数组中的k_diff数对
+    {
+        public class Solution
+        {
+            public int FindPairs(int[] nums, int k)
+            {
+                if (k < 0)
+                {
+                    return 0;
+                }
+                var res = 0;
+
+                if (nums.Length <= 1) { return 0; }
+
+                Array.Sort(nums);
+
+                Dictionary<int,int> map = new Dictionary<int,int>();
+
+                int i = 0;
+                int j = 1;
+                while (true)
+                {
+                    if (i >= nums.Length || j >= nums.Length)
+                        break;
+                    if (Math.Abs(nums[i] - nums[j]) < k)
+                    {
+                        j++;
+                    }
+                    else if (Math.Abs(nums[i] - nums[j]) > k)
+                    {
+                        i++;
+                        if(i == j)
+                        {
+                            j++;
+                        }
+                    }
+                    else
+                    {
+                        if (!map.ContainsKey(nums[i]))
+                        {
+                            res++;
+                            map[nums[i]] = nums[j];
+                        }
+                        i++;
+                        j++;
+                    }
+                }
+                return res;
+            }
+        }
+    }
+}
