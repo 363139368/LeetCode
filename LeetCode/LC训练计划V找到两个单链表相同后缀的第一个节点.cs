74ce1e3c47ad49997cb6395f1ba1320ecc6f1fb0
宋小华 <363139368@qq.com>
2025-03-12 20:25:41 +0800

    1

---
+++ LeetCode/LC训练计划V找到两个单链表相同后缀的第一个节点.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode
+{
+    //https://leetcode.cn/leetbook/read/illustration-of-algorithm/7fvoq2/
+    public class LC训练计划V找到两个单链表相同后缀的第一个节点
+    {
+        public class Solution
+        {
+            public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+            {
+                ListNode A = headA, B = headB;
+                while (A != B)
+                {
+                    A = (A != null) ? A.next : headB;
+                    B = (B != null) ? B.next : headA;
+                }
+                return A;
+            }
+        }
+    }
+}
