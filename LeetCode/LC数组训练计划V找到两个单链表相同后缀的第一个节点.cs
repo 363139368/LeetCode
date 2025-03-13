ab87ce006ceff086cc37e29cf06fac31d7045042
宋小华 <363139368@qq.com>
2025-03-13 20:55:41 +0800

    1

--- LeetCode/LC数组训练计划V找到两个单链表相同后缀的第一个节点.cs
+++ LeetCode/LC数组训练计划V找到两个单链表相同后缀的第一个节点.cs
@@ -3,7 +3,7 @@
 namespace LeetCode
 {
     //https://leetcode.cn/leetbook/read/illustration-of-algorithm/7fvoq2/
-    public class LC训练计划V找到两个单链表相同后缀的第一个节点
+    public class LC数组训练计划V找到两个单链表相同后缀的第一个节点
     {
         public class Solution
         {
