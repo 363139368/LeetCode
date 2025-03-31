2f4c3257bdb007bffbd134d0b121715e13cfa894
宋小华 <363139368@qq.com>
2025-03-31 18:39:08 +0800

    1

---
+++ LeetCode/225用队列实现栈.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class _225用队列实现栈 {
+        
+
+        
+        public class MyStack {
+
+            private Queue<int> queue = new Queue<int>();
+            public MyStack() {
+        
+            }
+    
+            public void Push(int x)
+            {
+                var c = queue.Count;
+                queue.Enqueue(x);
+                for (int i = 0; i < c; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+            }
+    
+            public int Pop() {
+                if(queue.Count == 0)
+                    return -1;
+                return queue.Dequeue();
+            }
+    
+            public int Top() {
+                if(queue.Count == 0)
+                    return -1;
+                return queue.Peek();
+            }
+    
+            public bool Empty() {
+                return queue.Count == 0;
+            }
+        }
+    }
+}
