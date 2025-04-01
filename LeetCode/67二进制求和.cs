033ee38e3eff97fe4fed98f9a141359c3abb0b2c
steven <363139368@qq.com>
2025-04-02 00:35:04 +0800

    1

--- LeetCode/67二进制求和.cs
+++ LeetCode/67二进制求和.cs
@@ -1,28 +1,19 @@
+using System.Collections.Generic;
+using System.Text;
+
 namespace LeetCode
 {
     public class _67二进制求和 {
-        public class Solution {
+        public class Solution
+        {
+            private char m_zero = '0';
+            private char m_one = '1';
             public string AddBinary(string a, string b) {
         
-                char[] chars = new char[a.Length + b.Length];
-                char[] chars2 = new char[(a.Length + b.Length)*2];
-
-                
-                while (true)
-                {
-                    
-                }
+  
             }
 
-            public void Sum(char[] a, char[] b,char[] chars, char[] chars2)
-            {
-                int i = 0;
-                while (true)
-                {
-                    var tmpa1 = a.Length - 1 - i >= 0 && a.Length - 1 - i < a.Length ? a[a.Length - 1 - i] : 0;
-                    i++;
-                }
-            }
+
         }
     }
 }
