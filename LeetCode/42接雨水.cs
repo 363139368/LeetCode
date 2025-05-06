fb96773526c0a2eeaa10a7fc9295729b92cbb7bc
dehua.song <363139368@qq.com>
2025-05-06 17:19:50 +0800

    111

--- LeetCode/42接雨水.cs
+++ LeetCode/42接雨水.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode
+﻿using System;
+
+namespace LeetCode
 {
     public class _42接雨水
     {
@@ -6,18 +8,27 @@
         {
             public int Trap(int[] height)
             {
-                int res = 0;
-                int i = 0;
-                int j = height.Length - 1;
-                int lv = 0;
-                int rv = 0;
-                while (i < j)
+                int[] tmp = new int[height.Length];
+                
+                int max = 0;
+                for (int i = 0; i < height.Length; i++)
                 {
-                    if (height[i] > lv)
+                    max = Math.Max(max, height[i]);
+                    tmp[i] = max;
+                }
+
+                int res = 0;
+                max = 0;
+                for (int i = height.Length - 1; i >= 0; i--)
+                {
+                    max = Math.Max(max, height[i]);
+                    int min = Math.Min(tmp[i],max);
+                    if (min > height[i])
                     {
-                        
+                        res += min -  height[i];
                     }
                 }
+                return res;
             }
         }
     }
