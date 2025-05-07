0d4147e3eb5cb5a09c5f5dcbc55300342faa4f27
dehua.song <363139368@qq.com>
2025-05-07 19:04:03 +0800

    1

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,8 +4,8 @@
     {
         private static void Main(string[] args)
         {
-            var tmp = new _15三数之和();
-            tmp.ThreeSum(new[] { -1,0,1,2,-1,-4 });
+            var tmp = new _438找到字符串中所有字母异位词();
+            tmp.FindAnagrams("cbaebabacd" ,"abc");
         }
     }
 }
